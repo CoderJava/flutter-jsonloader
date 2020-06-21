2d6bf237ba86e073089a78943f2f2058c8c628fc
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-06-21 22:37:26 +0700

    Added function getJsonAsMap

--- lib/jsonloader.dart
+++ lib/jsonloader.dart
@@ -1,5 +1,7 @@
 library jsonloader;
 
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class JSONLoader {
@@ -9,5 +11,14 @@ class JSONLoader {
   Future<String> getJsonAsString(String filename) async {
     var jsonString = rootBundle.loadString(filename);
     return jsonString;
+  }
+
+  /// Retrieve a map from the JSON file.
+  ///
+  /// Throws an exception if the file is not found.
+  Future<Map<String, dynamic>> getJsonAsMap(String filename) async {
+    var jsonString = await rootBundle.loadString(filename);
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    return jsonMap;
   }
 }
