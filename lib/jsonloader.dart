ea161bb703d04295615d4cc77c9182b1e05db0b1
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-06-21 22:29:17 +0700

    Create documentation for getJsonAsString function

--- lib/jsonloader.dart
+++ lib/jsonloader.dart
@@ -3,6 +3,9 @@ library jsonloader;
 import 'package:flutter/services.dart';
 
 class JSONLoader {
+  /// Retrieve a string from the JSON file.
+  ///
+  /// Throws an exception if the file is not found.
   Future<String> getJsonAsString(String filename) async {
     var jsonString = rootBundle.loadString(filename);
     return jsonString;
